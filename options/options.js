fa12bd12966b204ea02b3cde450ac138ea905d5f
Jill Mudgette <peoplewithpassions@gmail.com>
2023-10-30 01:48:46 -0700

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,15 +1,14 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "togetherwerave-43227",
-  url: "https://togetherwerave-43227.botics.co",
-  api: "https://togetherwerave-43227.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
-}
+  "name": "togetherwerave-43227",
+  "url": "https://togetherwerave-43227.botics.co",
+  "api": "https://togetherwerave-43227.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "Start"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
